a15f0077d66a5448ceada82b196e0763f1b925a3
superjacobl <jacoblower4@gmail.com>
2026-05-02 14:56:48 -0400

    fixed a few things

--- src/test/java/Michael.java
+++ src/test/java/Michael.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+// Michael made these tests
 
 public class Michael {
 
@@ -23,11 +24,13 @@ public class Michael {
 	@Test
 	void testIngridSameJava() {
 		var list1 = List.of("A", "B", "C", "D", "E", "F");
-        var list2 = List.of();
+		List<String> list2 = new ArrayList<>();
         var list3 = List.of("A", "B", "C");
+        var list4 = List.of("Z", "T", "F");
 
         assertFalse(Ingrid.same(list1, list2));
         assertFalse(Ingrid.same(list1, list3));
+        assertFalse(Ingrid.same(list3, list4));
         assertTrue(Ingrid.same(list3, list3));
 	}
 	
